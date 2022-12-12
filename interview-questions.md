5a9a97a2b33175cbc502bcde587d9d9b66099169
cibrown94 <cianibrown94@aol.com>
2022-12-12 08:05:53 -0800

    Week 7

--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,47 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: Well first, a foreign key, is like a primary key, but for a separate table. To fix this mistake go back and add the foreign key. 
 
-Researched answer:
+Researched answer: For starters a foreign key is a column in a table database, with a unique calues corresponding with another table. To make changes to update, change names, or schema files we would need to do a migration file. So for example we would run rails generate migration AddColumnToTable (required information)
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: Get, Show, Delete, Update, Destroy
 
-Researched answer:
+Researched answer: Passing params allows us as developers to add more info to our program by passing information into a route. Get, Show, Delete, Update all need to be passed param because these RESTful routes will target a specific, individual id. You would not used index, or create because those target the entire database. Index is the database as a whole. Create is making data to put into the index. 
 
 3. Name three rails generator commands. What is created by each?
 
-Your answer:
+Your answer: There is rails generate migrate (actionneeded). Rails generate 
 
-Researched answer:
+Researched answer: There are 3 rails generate command. Rails generate migrate, (this command will generate a migration file, which will allow the developer to make changes to the database. Like updating datas name, datas type, adding/removing a column.)
+Than there is rails generate model, (this will create a migration file, and update schema file, with the model name. This model generate command will be where we initate the columns and data type. For example, rails generate model Ciani address:string phone_number:interger). Lastly, there is rails generate rspec: install command, doing this will ensure all of the rspec dependencies are installed on the computer. The order is rails generate rspec:install, rails generate model, and rails migrate. 
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
 action: "GET" location: /students
+The get route will display of all of students. 
 
 action: "POST" location: /students
+The post route will create a new student.
 
 action: "GET" location: /students/new
+The get and with the students/new will return an HTML form for creating a new student
 
 action: "GET" location: /students/2
+This will display a specific student
 
 action: "GET" location: /students/2/edit
+Get and edit will return an HTML form for editing a student
 
 action: "PATCH" location: /students/2
+Patch will update a specific student
 
 action: "DELETE" location: /students/2
+Delete will delete a specific student
+
+
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -7,24 +7,47 @@
 # FILE: app/controller/blog_posts_controller.rb
 
 # ---1)
+# Taking place here in this code is 
+
+# BlogPostController is the controller. The controller is a logical center of an application. It will organize the communication between the user, views, and the model. A controller is made using $rails g controller (Controllername). In the next line, there is a def method with the name index. Index means to send back the entire list of data available for the model being queried. So the method's name is index, and index has a defintion. 
+
+
+
 class BlogPostsController < ApplicationController
   def index
+
+
     # ---2)
+
+# There is an instance variable named post is making an Active Record call on all of the BlogPost content and saving it in the post instance variable. 
+
     @posts = BlogPost.all
   end
 
   # ---3)
+
+# This is a method called show, that will display one item, by accessing it with a param id, after the variable instance of post. This will allow us to access an id inside of the item, and display/show it. 
   def show
     @post = BlogPost.find(params[:id])
   end
 
   # ---4)
+
+  # This is a method called new. New will allow users to add information into our web site, and store the information into the database. Create will allow the user to take data and add to it, to be in the database, creating a new instance. 
+
   def new
     @post = BlogPost.new
   end
 
   def create
+
+
     # ---5)
+
+# The instance of post which is BlogPost.all is allowing the users to create an instance whenever the user making a new entry under the blog_post_params in the Active Record. If the post meets all of the parameters, (being a strong param, meaning this will dictate the attributes allowed, so if all of the necessary items are met and deemed valid, than this will happen, which is essentially a conditional statement),than it will redirect to the _path. The path can link to another website, home screen, or confirmation screen,, etc. 
+
+# Than there is a def edit. Edit will be sends overs HTML forms, which will allow user to fill it out and send ouuut overwrite into a database. 
+
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -32,31 +55,52 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
+   
     # ---6)
+ 
+# Here we are again using the post instance, using the .find after BlogPost to find one item by its id. Since this is under the def edit, it is used to edit an existing model object. 
+
     @post = BlogPost.find(params[:id])
   end
 
+# Update just like edit will be updating a model class using the param id. 
+
   def update
     @post = BlogPost.find(params[:id])
+
+
     # ---7)
+
+    # Here will be updating/editing a a blog post update using a conditional statement that will trigger a redirect if the new entry is made correctly. If correct it will redirect to the attached link path. 
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
     end
   end
 
+  #  Here def destroy is looking for a param with a certain id. If the param meets the params id, than destroy it. 
+
   def destroy
     @post = BlogPost.find(params[:id])
     if @post.destroy
       # ---8)
+
+      # After looking through the Blog post, for a param: id a conditional statement was put in that if located, destroy the post from the database and redirect to a new link path. 
       redirect_to blog_posts_path
     end
   end
 
   # ---9)
+
+  # Using the private keyword restricts the scope of where a method can be called. Private is listed to a controller class, so that there is no chance that it can be called from somewhere else in the program. One private is set, everything under it is protected. So def blog_post_params is protected.
+  
   private
   def blog_post_params
+
+
     # ---10)
+
+    # Still under private. The require method makes sure that a specific parameter is provided, if it is not provided than it will display an error. So it requires a blog post. The permit method will return the params title, content, filtered.
     params.require(:blog_post).permit(:title, :content)
   end
 end
